1858c90e233e9add4e7073e754a9c8f9066238f1
Dhananjay Sharma <113139387+Dhnnjaysrma@users.noreply.github.com>
2023-07-10 19:08:18 +0530

    Create 111 Minimum Depth of Binary Tree.cpp

---
+++ 111 Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,21 @@
+ class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int ans = 1;
+        while (q.size()) {
+            int cnt = q.size();
+            while (cnt--) {
+                auto node = q.front();
+                q.pop();
+                if (!node->left && !node->right) return ans;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            ++ans;
+        }
+        return -1;
+    }
+};
